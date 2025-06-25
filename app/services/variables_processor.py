cd674917e6bcfa10364d56a11344767baaef9f29
mortenezzo <carlosja_15@hotmail.com>
2025-06-25 14:36:48 -0500

    ✍🏻 Fix variables_processor.py

--- app/services/variables_processor.py
+++ app/services/variables_processor.py
@@ -3,6 +3,7 @@ import os
 def append_launcher_to_variables(exe_path: str, app_name: str, circle_launcher_path: str) -> None:
     variables_file = os.path.join(circle_launcher_path, "@Resources", "Variables.inc")
     normalized_exe_path = os.path.normpath(exe_path)
+    replaced = False
 
     with open(variables_file, 'r', encoding="UTF-8") as f:
         content = f.readlines()
@@ -10,7 +11,10 @@ def append_launcher_to_variables(exe_path: str, app_name: str, circle_launcher_p
     with open(variables_file, 'w', encoding="UTF-8") as f:
         for line in content:
             if line.startswith(f"{app_name}="):
+                replaced = True
                 f.write(f"{app_name}=[\"{normalized_exe_path}\"]\n")
                 continue
             else:
                 f.write(line)
+        if not replaced:
+            f.write(f"{app_name}=[\"{normalized_exe_path}\"]\n")
