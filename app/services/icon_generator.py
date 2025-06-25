4457f4dcb65fc8ceccf55e9e141865752f2d5e81
mortenezzo <carlosja_15@hotmail.com>
2025-06-24 19:14:23 -0500

    ✍🏻 Improve icon generator

--- app/services/icon_generator.py
+++ app/services/icon_generator.py
@@ -1,13 +1,16 @@
-from PIL import Image, ImageOps, ImageEnhance
 import os
+from pathlib import Path
+from PIL import Image, ImageOps, ImageEnhance
 
-def generate_3state_icon(image_path: str, output_path: str = None) -> str:
+def generate_3state_icon(image_path: str, app_name: str, rainmeter_path: Path) -> str:
     """
     Generate a 3-state icon from the given image.
-    :param image_path: Path to the input image.
-    :param output_path: Path to save the generated icon. If None, it will save in the same directory as the input image with a .png extension.
+    :param image_path: Path to the original PNG image.
+    :param app_name: Name of the application to use for the icon.
+    :param rainmeter_path: Path to the Rainmeter skins directory.
     :return: Path to the generated icon.
     """
+    app_path = rainmeter_path.joinpath(app_name)
     original = Image.open(image_path).convert("RGBA")
     w, h = original.size
 
@@ -19,9 +22,8 @@ def generate_3state_icon(image_path: str, output_path: str = None) -> str:
     result.paste(hovered, (w, 0))
     result.paste(down, (w * 2, 0))
 
-    if output_path is None:
-        base = os.path.splitext(image_path)[0]
-        output_path = base + ".png"
+    output_path = app_path.joinpath(f"{app_name}.png")
 
     result.save(output_path)
-    return output_path
+
+    return str(output_path)
